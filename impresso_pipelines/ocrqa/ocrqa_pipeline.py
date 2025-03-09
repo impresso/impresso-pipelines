9bc3c01f3a7b2e49e435d90f456cb23dd43e75e2
Gleb Vinarskis <s215226@dtu.dk>
2025-03-09 18:51:09 +0100

    returing ocr pipeline code

--- impresso_pipelines/ocrqa/ocrqa_pipeline.py
+++ impresso_pipelines/ocrqa/ocrqa_pipeline.py
@@ -1,67 +1,156 @@
-"""
-This module provides a language identification pipeline using the floret model.
-"""
-
+from huggingface_hub import hf_hub_download
+from transformers import Pipeline, AutoModelForSequenceClassification, AutoTokenizer
+from transformers.pipelines import PIPELINE_REGISTRY, SUPPORTED_TASKS
+from typing import List, Dict, Union, Optional, Tuple
+import unicodedata
+from huggingface_hub import hf_hub_download
+from pybloomfilter import BloomFilter
+import unicodedata
 from typing import Optional
 from huggingface_hub import hf_hub_download, list_repo_files
-import floret
+from pybloomfilter import BloomFilter
 import re
 
-class LangIdentPipeline:
-    """
-    A pipeline for language identification using a pre-trained floret model.
-    """
-    
-    def __init__(self, model_id: Optional[str] = None, repo_id: str = "impresso-project/impresso-floret-langident", revision: str = "adding_emas_pipeline"):
-        """
-        Initialize the LangIdentPipeline with the specified or newest model from the repository.
+from glebs_package.langident.langident_pipeline import LangIdentPipeline
 
-        Args:
-            model_id (str, optional): The specific model file to use. If not provided, the newest model will be used.
-            repo_id (str): The repository ID on Hugging Face Hub.
-            revision (str): The revision of the repository.
-        """
-        if model_id is None:
-            repo_files = list_repo_files(repo_id, revision=revision)
-            model_files = [file for file in repo_files if re.match(r"langident-v\d+\.\d+\.\d+\.bin", file)]
-            if not model_files:
-                raise ValueError("No model files found in the repository.")
+
+
+
+def get_bloomfilter(model_id: str, filename: str):
+        return BloomFilter.open(hf_hub_download(repo_id=model_id, filename=filename))
+
+class OCRQAPipeline:   
+    def __init__(self):
+        self.SUPPORTED_LANGUAGES = self.get_supported_languages()
+
+    def get_supported_languages(self) -> set:
+        repo_files = list_repo_files("impresso-project/OCR-quality-assessment-unigram")
+        languages = {file.split('-')[-1].split('.')[0] for file in repo_files if file.startswith("ocrqa-wp_v")}
+        
+        return languages
+
+    def __call__(self, text, language=None, version=None, diagnostics=False, model_id=False, supported_languages=False):
+        self.language = language
+        self.version = version
+        self.diagnostics = diagnostics
+        self.model_id = model_id
+        self.supported_languages = supported_languages
+        
+        if self.language is None:
+            lang_model = LangIdentPipeline()
+            lang_result = lang_model(text)
+            self.language = lang_result["language"]
+
+        if self.language not in self.SUPPORTED_LANGUAGES:
+            raise ValueError(f"Unsupported language: {self.language}")
+
+        if self.version is None:
+            repo_files = list_repo_files("impresso-project/OCR-quality-assessment-unigram")
+            versions = [
+                re.search(r"_v(\d+\.\d+\.\d+)", file).group(1)
+                for file in repo_files
+                if file.startswith("ocrqa-wp_v") and file.endswith(f"-{self.language}.bloom")
+            ]
             
-            # Sort model files by version and select the newest one
-            model_files.sort(key=lambda x: list(map(int, re.search(r"v(\d+\.\d+\.\d+)", x).group(1).split('.'))), reverse=True)
-            model_id = model_files[0]
+            # versions = [v for v in versions if all(part.isdigit() for part in v.split('.'))]
+            self.version = max(versions, key=lambda v: list(map(int, v.split('.'))))
+            
 
-        model_path = hf_hub_download(repo_id=repo_id, filename=model_id, revision=revision)
-        self.model = floret.load_model(model_path)
-        self.model_name = model_id
+        bf = get_bloomfilter("impresso-project/OCR-quality-assessment-unigram", f"ocrqa-wp_v{self.version}-{self.language}.bloom")
 
-    def __call__(self, text: str, diagnostics: bool = False, model_id: bool = False) -> dict:
+        output = self.filter_text(text, bf)
+
+        if self.supported_languages:
+            output["supported_languages"] = list(self.SUPPORTED_LANGUAGES)
+
+        return output
+
+    # Define normalization table
+    QUOTES_PUNCT = "„•<>!\"#%&'’"
+    ASCII_PUNCT = "()*,./:;?"
+    BRACKETS_SPECIAL = "[]\\~_{}"
+    UNICODE_PUNCT = "\xa1\xab\xb7\xbb\xbf"
+    DASH_CARET = "—^`"
+    SPECIAL_SYMBOLS = "¦§£="
+    HYPHEN = "-"
+    DIGITS = "0123456789"
+
+    NORMALIZATION_TABLE = str.maketrans(
+        {
+            char: " "
+            for char in (
+                QUOTES_PUNCT
+                + ASCII_PUNCT
+                + BRACKETS_SPECIAL
+                + UNICODE_PUNCT
+                + DASH_CARET
+                + SPECIAL_SYMBOLS
+                + HYPHEN
+            )
+        }
+        | {char: "0" for char in DIGITS}
+    )
+
+
+    def normalize_text(self, s: str, unicode_normalize: Optional[str] = "NFKC") -> str:
+        """Normalize text by replacing punctuation with spaces and digits with '0'."""
+        if unicode_normalize:
+            s = unicodedata.normalize(unicode_normalize, s).lower()
+        return s.translate(self.NORMALIZATION_TABLE)
+
+
+    def filter(self, text: str, bloom_filter: BloomFilter):
+        # Normalize and tokenize text
+        normalized_text = self.normalize_text(text)
+        tokens = normalized_text.split()
+
+        # Check tokens against the bloom filter
+        for token in tokens:
+            if self.diagnostics:
+                if token in bloom_filter:
+                    print(f"'{token}' is in the bloom filter.")
+                else:
+                    print(f"'{token}' is NOT in the bloom filter.")
+
+
+    def filter_text(self, DE_TEXT: str, bloom_filter: BloomFilter) -> Dict[str, Union[str, float, Dict[str, Union[List[str], str]]]]:
         """
-        Identify the language of the given text.
+        Filter the text using the bloom filter and return the score and diagnostics if enabled.
 
         Args:
-            text (str): The input text to identify the language for.
-            diagnostics (bool): Whether to include diagnostic information in the output.
-            model_id (bool): Whether to include the model name in the output.
+            DE_TEXT (str): The input text to filter.
+            bloom_filter (BloomFilter): The bloom filter to use for filtering.
 
         Returns:
-            dict: The identified language code, score, and optionally diagnostics and model name.
+            Dict[str, Union[str, float, Dict[str, Union[List[str], str]]]]: The output containing language, score, and optionally diagnostics.
         """
-        output = self.model.predict(text, k=300 if diagnostics else 1)
-        language, value = output
-  
-        value = [round(num, 2) for num in value]
-        
-        score = value[0]
-    
+        knowns = set()
+        unknowns = set()
 
-        result = {"language": language[0].replace("__label__", ""), "score": score}
+        # Normalize and tokenize text
+        normalized_text = self.normalize_text(DE_TEXT)
+        tokens = normalized_text.split()
 
-        if diagnostics:
-            language_dist = [{"language": lang.replace("__label__", ""), "score": val} for lang, val in zip(language, value)]
-            result["diagnostics"] = {"language_dist": language_dist}
+        # Check tokens against the bloom filter
+        for token in tokens:
+            if token in bloom_filter:
+                knowns.add(token)
+            else:
+                unknowns.add(token)
 
-        if model_id:
-            result["model_name"] = self.model_name
+        # Compute the score
+        score = len(knowns) / (len(knowns) + len(unknowns)) if (len(knowns) + len(unknowns)) > 0 else 0
+        score = round(score, 1)
 
-        return result
+        output = {"language": self.language, "score": score}
+
+        if self.diagnostics:
+            output["diagnostics"] = {
+                "known_tokens": list(knowns),
+                "unknowns_tokens": list(unknowns),
+                "bloom_filter": f"ocrqa-wp_v{self.version}-{self.language}.bloom"
+            }
+        elif self.model_id:
+            output["bloom_filter"] = f"ocrqa-wp_v{self.version}-{self.language}.bloom"
+
+        return output
