9d1c6861e41fab10a918b5febc96a68adcadbb65
Gleb Vinarskis <s215226@dtu.dk>
2025-03-11 20:43:08 +0100

    fixed name to workflows

--- impresso_pipelines/langident/langident_pipeline.py
+++ impresso_pipelines/langident/langident_pipeline.py
@@ -58,8 +58,8 @@ class LangIdentPipeline:
         result = {"language": language[0].replace("__label__", ""), "score": score}
 
         if diagnostics:
-            language_dict = [{"language": lang.replace("__label__", ""), "score": val} for lang, val in zip(language, value)]
-            result["diagnostics"] = {"language_dict": language_dict}
+            language_dist = [{"language": lang.replace("__label__", ""), "score": val} for lang, val in zip(language, value)]
+            result["diagnostics"] = {"language_dict": language_dist}
 
         if model_id:
             result["model_name"] = self.model_name
