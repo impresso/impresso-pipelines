bfc071e8e6fad9d05842c88650239c32c912aebb
Maslionok <bashmak2002@gmail.com>
2025-03-23 12:19:27 +0100

    automatic checking of requirements for mallet

--- impresso_pipelines/mallet/__init__.py
+++ impresso_pipelines/mallet/__init__.py
@@ -1,12 +1,25 @@
-try:
-    import huggingface_hub
-    import floret
-    import spacy
-    
-    # Only import this after checking dependencies
-    from .mallet_pipeline import MalletPipeline
-except ImportError:
-    raise ImportError(
-        "The mallet subpackage requires additional dependencies. "
-        "Please install them with: pip install 'impresso-pipelines[mallet]'"
+import importlib
+import sys
+
+REQUIRED_PACKAGES = [
+    "jpype",
+    "smart_open",
+    "boto3",
+    "dotenv",
+    "spacy",
+]
+
+MISSING_PACKAGES = []
+
+for package in REQUIRED_PACKAGES:
+    if importlib.util.find_spec(package) is None:
+        MISSING_PACKAGES.append(package)
+
+if MISSING_PACKAGES:
+    missing = ", ".join(MISSING_PACKAGES)
+    sys.stderr.write(
+        f"Error: The following required packages are missing for the Mallet module: {missing}\n"
+        "Please install them with: pip install 'impresso-pipelines[mallet]'\n"
     )
+    sys.exit(1)
+
