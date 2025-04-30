46b4abc91329ea0c8be08e3c819d7014145e0b25
Gleb Gleb <maslionok@Glebs-MacBook-Pro.local>
2025-04-30 13:48:08 +0200

    added extra parameters and fixed some naming

--- impresso_pipelines/ldatopics/config.py
+++ impresso_pipelines/ldatopics/config.py
@@ -15,3 +15,9 @@ TOPIC_MODEL_DESCRIPTIONS = {
     "fr": "https://huggingface.co/impresso-project/mallet-topic-inferencer/resolve/main/models/tm/tm-fr-all-v2.0.topic_model_topic_description.jsonl.bz2",
     "lb": "https://huggingface.co/impresso-project/mallet-topic-inferencer/resolve/main/models/tm/tm-lb-all-v2.1.topic_model_topic_description.jsonl.bz2",
 }
+
+TOPIC_MODEL_DESCRIPTIONS_HF = {
+    "de": ["impresso-project/mallet-topic-inferencer", "models/tm/tm-de-all-v2.0.topic_model_topic_description.jsonl.bz2"],
+    "fr": ["impresso-project/mallet-topic-inferencer", "models/tm/tm-fr-all-v2.0.topic_model_topic_description.jsonl.bz2"],
+    "lb": ["impresso-project/mallet-topic-inferencer", "models/tm/tm-lb-all-v2.1.topic_model_topic_description.jsonl.bz2"],
+}
--- test.py
+++ test.py
@@ -4,18 +4,22 @@ from impresso_pipelines.ldatopics.mallet_pipeline import LDATopicsPipeline
 
 
 text = "Die Katzen spielten fröhlich im Garten, während die Vögel sangen."
+text_new = ("Die Katzen spielten fröhlich im Garten,\n\n"
+    "während die Vögel sangen.")
+
+
 text_fr = "Les chats jouaient joyeusement dans le jardin pendant que les oiseaux chantaient."
 text_lb = "Katzen hunn am Gaart gespillt, während Villercher gesongen hunn."
 mallet = LDATopicsPipeline()
-print(mallet(text, doc_name = "my_doc"))
+print(mallet(text, min_p=0.01))
 # print empty line
 print()
 print()
 print()
 # print(mallet(text_fr))
-print(mallet(text_lb))
+# print(mallet(text_lb))
 
-print(mallet(text_lb, doc_name = "another_doc"))
+# print(mallet(text_lb, doc_name = "another_doc"))
 
 
-print(mallet(text_lb))
+# print(mallet(text_lb))
